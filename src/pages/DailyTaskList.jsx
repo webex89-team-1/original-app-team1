940ba2e075ba1f876b774f431bc428f97139f4a5
manabi43 <manabi4323@gmail.com>
2025-08-24 19:51:32 +0900

    新しいカードを作る途中

--- src/pages/DailyTaskList.jsx
+++ src/pages/DailyTaskList.jsx
@@ -1,19 +1,17 @@
 export default function DailyTaskList() {
   const handlebutton = () => {
-    alert("ボタンが押されたよ");
+    alert("新しいTo doを作成するよ");
   };
   return (
     <div>
       <h1>一日のやることリスト</h1>
-      <button onClick={handlebutton}>ボタン</button>
+      <button onClick={handlebutton}>To do</button>
     </div>
   );
 }
 
-// const checkbox = document.getElementById("checkbox")
-// //idが"checkbox"のタグを取得
-
-// //checkboxを押した時の処理（checkboxが押された時に発動する）
-// checkbox.onclick = function(){
-//     console.log("チェックボックスが押されたよ！")//コンソールに出力
-// }
+function App() {
+  const [cards, setCards] = useState([
+    { id: crypto.randomUUID(), text: "メモ1" },
+  ]);
+}
