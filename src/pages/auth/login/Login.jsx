418bf78d640436728049fddabc213549d12cf6b6
Ashunar0 <asahi.gaia1530@gmail.com>
2025-08-26 16:29:07 +0900

    Authentication function implemented

---
+++ src/pages/auth/login/Login.jsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/src/components/auth/login/LoginForm";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/pages/auth/register/Register.jsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "@/src/components/auth/register/RegisterForm";
+import "./Register.css";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <RegisterForm />
+    </div>
+  );
+}
